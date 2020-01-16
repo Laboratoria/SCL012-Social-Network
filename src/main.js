1bc846fa240e905c75aea496c5d272cc5a7ee79b
JocelynJofreAntoine <joce.jofreantoine@gmail.com>
2020-01-16 10:04:19 -0300

    input de registro

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+//import { MyFunction } from './lib/index.js';
 
-myFunction();
+function registro(){
+    let nombre = document.getElementById('nombre').Value;
+    let email = document.getElementById('email').Value;
+    let contrasena = document.getElementById('contrasena').Value;
+
+    console.log()
+
+}
