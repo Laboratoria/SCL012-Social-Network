2ffbd3dda941abbb89a86a8c36a224f163d5152f
MonicaBartha <barthamonica@gmail.com>
2020-01-17 13:45:28 -0300

    funcinon login firebase

--- src/main.js
+++ src/main.js
@@ -1,20 +1,101 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Este es el punto de entrada de tu aplicacion
-
-
 //import { MyFunction } from './lib/index.js';
 
-function registro(){
-    let nombre = document.getElementById('nombre').Value;
-    let email = document.getElementById('email').Value;
-    let contrasena = document.getElementById('contrasena').Value;
+// ------ Initialize Cloud Firestore through Firebase
+// Base de datos de Firebase
+firebase.initializeApp({
+    apiKey: 'AIzaSyAewAJTAGvRk5IyI8jCQ3l3DVsquWzRJVk',
+    authDomain: 'https://finger-food.firebaseapp.com/',
+    projectId: 'finger-food'
+  });
+ // BASE DE DATOS 
+  let database = firebase.firestore(); 
+// Add data --  Usuarios
+database.collection("users").add({
+    user: "username",
+    email: "email",
+    password: "password"
+})
+.then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+})
+.catch(function(error) {
+    console.error("Error adding document: ", error);
+});
+// Read Data -- esto lee los datos ingresados 
+database.collection("users").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+    });
+});
+// Sign up a new user -- Registro de usuario
+firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    let errorCode = error.code;
+    let errorMessage = error.message;
+    // ...
+  });
 
-    console.log()
 
+// function de login para usuarios existentes
+let loginBtn = document.getElementById('loginBtn');
+    loginBtn.addEventListener("click", function login() {
+        let email = document.getElementById('email').value;
+        let contrasena = document.getElementById('contrasena').value;
+
+        firebase.auth().signInWithEmailAndPassword(email, contrasena)
+        .catch(function(error) {
+            // Handle Errors here.
+            let errorCode = error.code;
+            let errorMessage = error.message;
+            console.log(errorCode);
+            console.log(errorMessage);
+          });
+})
+// observador: authentication state observer and get user data
+function observador(){
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      let displayName = user.displayName;
+      let email = user.email;
+      let emailVerified = user.emailVerified;
+      let photoURL = user.photoURL;
+      let isAnonymous = user.isAnonymous;
+      let uid = user.uid;
+      let providerData = user.providerData;
+      // ...
+    } else {
+      // User is signed out.
+      console.log('no existe usuario activo')
+    }
+  });
 }
-=======
-/*
-import { myFunction } from './lib/index.js';
+observador();
 
-myFunction();
-*/
+
 
