41a7afcea054f569ce97d1260a4131635e44649b
Ruth Maureira <ruthalemau@gmail.com>
2020-01-28 05:42:47 -0300

    Página post y cerrar sesión

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 import {
   signInNew,
   signIn,
+  signOff,
 } from './lib/index.js';
 
 /*---------------------------------------------------------------------------------*/
@@ -9,40 +10,94 @@ import {
 function init() {
   const root = document.getElementById('root');
   /* Formulario login */
-  root.innerHTML = `
-  <section class="login" id="login">
-    <img src="img/logo2.png" alt="logo Finger Food" class="login__logo">
-    <h1 class="login__title">Bienvenid@!</h1>
-    <form class="login__form">
-      <div class="login__container">
-        <div class="login__inputMail">
-          <i class="fas fa-envelope icon"></i>
-          <input type="email" class="login__inputText" id="login__email" placeholder="Correo Electrónico">
-        </div>
-        <div class='login__inputPassword'>
-          <i class='fas fa-key icon'></i>
-          <input type="password" class="login__inputPass" id="login__pass" placeholder="Contraseña">
-        </div>
-        <p class="login__acceptService">Al continuar, aceptas nuestras condiciones del servicio.</p>
-        <input type="checkbox" name="remember" id="login__remember" value="remember"> Recuérdame
-        <input type="submit" value="Acceder" id="login__accept" class="login__button">
-        <p class="login__forget">¿Olvidó su contraseña?<a class="login__linkRecover" id="login__recover" href="#">Recuperar</a></p>
-        </div>
-    </form>
-    <div class="login__divider">
-      <hr>
-      <span>o</span>
-      <hr>
+  function start() {
+    root.innerHTML = `
+    <section class="login" id="login">
+      <img src="img/logo2.png" alt="logo Finger Food" class="login__logo">
+      <h1 class="login__title">Bienvenid@!</h1>
+      <form class="login__form">
+        <div class="login__container">
+          <div class="login__inputMail">
+            <i class="fas fa-envelope icon"></i>
+            <input type="email" class="login__inputText" id="login__email" placeholder="Correo Electrónico">
+          </div>
+          <div class='login__inputPassword'>
+            <i class='fas fa-key icon'></i>
+            <input type="password" class="login__inputPass" id="login__pass" placeholder="Contraseña">
+          </div>
+          <p class="login__acceptService">Al continuar, aceptas nuestras condiciones del servicio.</p>
+          <input type="checkbox" name="remember" id="login__remember" value="remember"> Recuérdame
+          <input type="submit" value="Acceder" id="login__accept" class="login__button">
+          <p class="login__forget">¿Olvidó su contraseña?<a class="login__linkRecover" id="login__recover" href="#">Recuperar</a></p>
+          </div>
+      </form>
+      <div class="login__divider">
+        <hr>
+        <span>o</span>
+        <hr>
+      </div>
+      <button type="button" name="btn__google" class="btn__rrss" id="login__googleBtn">
+        <img src="img/googleColor.svg" alt="logo Google" class="btn__icon">
+      </button>
+      <button type="button" name="btn_facebook" class="btn__rrss" id="login__facebookBtn">
+        <img src="img/facebook.svg" alt="logo Google" class="btn__icon">
+      </button>
+      <p class="login__acceptService">Al continuar, aceptas nuestras condiciones del servicio.</p>
+      <p class="login__register">¿No tienes una cuenta?<a class="login__linkRegister" id="login__btnRegister" href="#">Regístrate</a></p>
+    </section>`;
+  }
+
+  start();
+
+  function newPage() {
+    root.innerHTML = `
+    <nav class="navi">
+    <img src="img/logo2.png" alt="logo" class="logoNav">
+    <div class="navigation">
+      <ul class="navigation__list">
+        <li class="navigation__item"><a href="#divSearch"><i class="fas fa-search icon"></i></a></li>
+        <li class="navigation__item"><a href="#"><i class="fas fa-plus icon"></i></a></li>
+        <li class="navigation__item"><a href="#"><i class="fas fa-user-circle fa-2x icon"></i></a></li>
+        <li class="navEmail"> user.email </li>
+        <li class="navigation__item icon"><a id="closeSession" href="#"><i class="far fa-times-circle fa-2x icon"></i></a></li>
+      </ul>
     </div>
-    <button type="button" name="btn__google" class="btn__rrss" id="login__googleBtn">
-      <img src="img/googleColor.svg" alt="logo Google" class="btn__icon">
-    </button>
-    <button type="button" name="btn_facebook" class="btn__rrss" id="login__facebookBtn">
-      <img src="img/facebook.svg" alt="logo Google" class="btn__icon">
-    </button>
-    <p class="login__acceptService">Al continuar, aceptas nuestras condiciones del servicio.</p>
-    <p class="login__register">¿No tienes una cuenta?<a class="login__linkRegister" id="login__btnRegister" href="#">Regístrate</a></p>
-  </section>`;
+  </nav>
+
+  <section class="main">
+    <h1 class="welcome">Bienvenid@ <span> user.displayName </span> </h1>
+    <div class="typeSelect">
+      <select id="typeFood" class="typeFood">
+        <option value="all">Tipos de comida</option>
+        <option value="rapida">Comida Rápida</option>
+        <option value="saludable">Comida Saludable</option>
+        <option value="China">Comida China</option>
+        <option value="mexicana">Comida Mexicana</option>
+        <option value="peruana">Comida Peruana</option>
+      </select>
+    </div>
+    <div class="inputSearch" id="divSearch">
+       <i class="fas fa-search icon"></i>
+       <input type="text" class="inputText" id="inputSearch" placeholder="Buscar">
+     </div>
+  </section>
+  <div id="wrap" class="wrap"></div>
+  <footer id="contact" class="contact">
+    <p> Finger Food 2020. Todos los derechos reservados.</p>
+  </footer>`;
+
+    /* Cerrar sesión */
+    const closeSession = document.getElementById('closeSession');
+
+    closeSession.addEventListener('click', () => {
+      /* cerrar sesión de usuario */
+      signOff();
+      /* Pasar a página inicial */
+      start();
+    });
+
+  }
+
 
   /* Ingreso usuario existente */
   const emailIngreso = document.getElementById('login__email');
@@ -55,10 +110,6 @@ function init() {
     signIn(emailIngreso.value, passIngreso.value);
     /* Pasar a página de post */
   });
-
-  function newPage() {
-    root.innerHTML = 'Solo lo ve usuario ACTIVO';
-  }
 
   function observador() {
     firebase.auth().onAuthStateChanged(function (user) {
